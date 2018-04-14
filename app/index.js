bc327b85b99db64a34b8546e3fb39d4fb06891be
motosharpley <motosharpley@gmail.com>
2018-04-13 23:05:14 -0500

    parse url and get the path

--- app/index.js
+++ app/index.js
@@ -8,16 +8,20 @@ const http = require('http');
 const url = require('url');
 
 // The server should respond to all requests with a string
-const server = http.createServer(function(req, res) {
+const server = http.createServer(function(req,res) {
     
     // Get the URL and parse it 
+    const parsedUrl = url.parse(req.url,true);
 
     // Get the path
+    const path = parsedUrl.pathname;
+    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
     
     // Send the response
     res.end('Hello Node\n');
 
     // Log the request path
+    console.log('Request recieved on path: '+trimmedPath);
     
 })
 
