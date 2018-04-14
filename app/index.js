d3a04840802f53caa5da43c3de0ff93d3a5f229d
motosharpley <motosharpley@gmail.com>
2018-04-14 15:02:57 -0500

    parse request payload

--- app/index.js
+++ app/index.js
@@ -6,37 +6,54 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string
-const server = http.createServer(function(req,res) {
-    
-    // Get the URL and parse it 
-    const parsedUrl = url.parse(req.url,true);
+const server = http.createServer(function(req, res) {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true);
 
-    // Get the path
-    const path = parsedUrl.pathname;
-    const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
-    // Get the query string as an object
-    const queryStringObject = parsedUrl.query;
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
 
-    // Get the HTTP Method
-    const method = req.method.toUpperCase();
+  // Get the HTTP Method
+  const method = req.method.toUpperCase();
 
-    // Get the headers as an object
-    const headers = req.headers;
-    
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the paylod if there is one
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', function(data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function() {
+    buffer += decoder.end();
+
     // Send the response
     res.end('Hello Node\n');
 
     // Log the request path
-    console.log(`Request recieved on path: ${trimmedPath} with the http method: ${method} with these query string params`, queryStringObject);
+    console.log(
+      `Request recieved on path: ${trimmedPath} with the http method: ${method} with these query string params`,
+      queryStringObject
+    );
 
     // Log the request headers
-    console.log('Request was recieved with these headers:',headers);
-})
+    console.log('Request was recieved with these headers:', headers);
+
+    // Log the request payload
+    console.log('Request was recieved with this payload:', buffer);
+    
+  });
+});
 
 // Start the server, and listen on PORT 3000
-server.listen(3000,function(){
-    console.log("The server is listening on port 3000")
-})
+server.listen(3000, function() {
+  console.log('The server is listening on port 3000');
+});
