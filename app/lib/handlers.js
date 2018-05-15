e8188e2ceb35c77886830b79259ac3365a1d4730
Bryan Sharpley <motosharpley@gmail.com>
2018-05-15 11:00:31 -0500

    validate that all data is present in post request

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -5,6 +5,7 @@
 */
 
 // Dependencies
+const _data = require('./data');
 
 // Define handlers
 const handlers = {};
@@ -33,8 +34,29 @@ handlers._users.get = function(data,callback){
 }
 
 // Users - POST
+// Required data: firstName, lastName, phone, password, tosAgreement
+// Optional data: none
 handlers._users.post = function(data,callback){
+  // Check that all required fields are present
+  const firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+  const lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+  const phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+  const password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+  const tosAgreement = typeof(data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false;
 
+  if(firstName && lastName && phone && password && tosAgreement){
+    // Make sure user doesn't already exist
+    _data.read('users',phone,function(err,data){
+      if(err){
+        // Hash the password
+        const hashPassword = helpers.hash(password);
+      } else {
+        callback(400,{'Error' : 'User with that phone number already exists'});
+      }
+    })
+  } else {
+    callback(400,{'Error' : 'Missing Required Fields'});
+  }
 }
 
 // Users - PUT
@@ -44,7 +66,7 @@ handlers._users.put = function(data,callback){
 
 // Users - DELETE
 handlers._users.delete = function(data,callback){
-  
+
 }
 
 // Not found handler
