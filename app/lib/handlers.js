9c94a3a2179363ccd367aa72af9be18ea39d2056
Bryan Sharpley <motosharpley@gmail.com>
2018-05-17 19:07:07 -0500

    add tokens post handler

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -189,7 +189,6 @@ handlers._users.delete = function(data,callback){
 }
 
 /*
-*
 * START OF TOKENS HANDLERS
 */
 // Tokens
@@ -211,8 +210,45 @@ handlers._tokens.get = function(data,callback){
 }
 
 // Tokens - POST
+// Required data: phone, password
+// Optional data: none
 handlers._tokens.post = function(data,callback){
-  
+  const phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+  const password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+  if(phone && password){
+    // Lookup user by phone number
+    _data.read('users', phone,function(err,userData){
+      if(!err && userData){
+        // Hash the sent password and compare to password stored in user object
+        const hashedPassword = helpers.hash(password);
+        if(hashedPassword == userData.hashedPassword){
+          // If valid create a new token with a random name, Set expiration 1 hour in the future
+          const tokenId = helpers.createRandomString(20);
+          const expires = Date.now() + 1000 * 60 * 60;
+          const tokenObject = {
+            'phone' : phone,
+            'id' : tokenId,
+            'expires' : expires
+          }
+          // Store the token
+          _data.create('tokens',tokenId,tokenObject,function(err){
+            if(!err){
+              callback(200,tokenObject);
+            } else {
+              callback(500,{'Error' : 'Could not create new token'});
+            }
+          })
+        } else {
+          callback(400,{'Error' : 'Invalid Password'});
+        }
+      } else {
+        callback(400,{'Error' : 'Could not find specified user'});
+      }
+    })
+
+  } else {
+    callback(400,{'Error' : 'Missing required field(s)'});
+  }
 }
 
 // Tokens - PUT
