3fdb1f14c2c08f90795a335d9b6695d5f08bb8fa
Bryan Sharpley <motosharpley@gmail.com>
2018-07-19 08:26:11 -0500

    add list function

--- app/lib/data.js
+++ app/lib/data.js
@@ -100,6 +100,21 @@ lib.delete = function(dir,file,callback){
   })
 }
 
+// List all items in a directory
+lib.list = function(dir,callback){
+  fs.readdir(lib.baseDir+'dir'+'/',function(err,data){
+    if(!err && data && data.length > 0){
+      let trimmedFileNames = [];
+      data.forEach(function(fileName){
+        trimmedFileNames.push(fileName.replace('.json',''));
+      })
+      callback(false,trimmedFileNames);
+    } else {
+      callback(err,data);
+    }
+  })
+}
+
 
 
 // Export the module
