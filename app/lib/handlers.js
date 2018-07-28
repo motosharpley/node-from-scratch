9f3fe507438b79f8e9c4abf2c6bbe80bfeee2eef
Bryan Sharpley <motosharpley@gmail.com>
2018-07-27 19:34:48 -0500

    add public handler

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -67,7 +67,7 @@ handlers.favicon = function(data,callback){
   // Reject any method that is not GET
   if(data.method == 'get'){
     // Read in the favicon's data
-    helpers.getStaticAssets('favicon.ico',function(err,data){
+    helpers.getStaticAsset('favicon.ico',function(err,data){
       if(!err && data){
         // callback the data
         callback(200,data,'favicon');
@@ -76,6 +76,50 @@ handlers.favicon = function(data,callback){
       }
     })
 
+  } else {
+    callback(405);
+  }
+}
+
+// Public assets
+handlers.public = function(data,callback){
+  // Reject any regeust that is not GET
+  if(data.method == 'get'){
+    // Get the filename being requested
+    let trimmedAssetName = data.trimmedPath.replace('public/','').trim();
+    if(trimmedAssetName.length > 0){
+      // Read in the asset's data
+      helpers.getStaticAsset(trimmedAssetName,function(err,data){
+        if(!err && data){
+          // Determine the content type (default to plain text)
+          let contentType = 'plain';
+
+          if(trimmedAssetName.indexOf('.css') > -1){
+            contentType = 'css';
+          }
+
+          if(trimmedAssetName.indexOf('.png') > -1){
+            contentType = 'png';
+          }
+
+          if(trimmedAssetName.indexOf('.jpg') > -1){
+            contentType = 'jpg';
+          }
+
+          if(trimmedAssetName.indexOf('.ico') > -1){
+            contentType = 'favicon';
+          }
+
+          // Callback the data
+          callback(200,data,contentType);
+
+        } else {
+          callback(404)
+        }
+      })
+    } else {
+      callback(404);
+    }
   } else {
     callback(405);
   }
