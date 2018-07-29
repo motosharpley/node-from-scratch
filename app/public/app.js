e471a7d4caeefee4e42deaf60e3770037d3148f7
Bryan Sharpley <motosharpley@gmail.com>
2018-07-29 16:45:51 -0500

    refactor var ... doh

--- app/public/app.js
+++ app/public/app.js
@@ -86,19 +86,19 @@ app.bindForms = function () {
 
     // Stop it from submitting
     e.preventDefault();
-    var formId = this.id;
-    var path = this.action;
-    var method = this.method.toUpperCase();
+    let formId = this.id;
+    let path = this.action;
+    let method = this.method.toUpperCase();
 
     // Hide the error message (if it's currently shown due to a previous error)
     document.querySelector("#" + formId + " .formError").style.display = 'hidden';
 
     // Turn the inputs into a payload
-    var payload = {};
-    var elements = this.elements;
-    for (var i = 0; i < elements.length; i++) {
+    let payload = {};
+    let elements = this.elements;
+    for (let i = 0; i < elements.length; i++) {
       if (elements[i].type !== 'submit') {
-        var valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
+        let valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
         payload[elements[i].name] = valueOfElement;
       }
     }
@@ -109,7 +109,7 @@ app.bindForms = function () {
       if (statusCode !== 200) {
 
         // Try to get the error from the api, or set a default error message
-        var error = typeof (responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
+        let error = typeof (responsePayload.Error) == 'string' ? responsePayload.Error : 'An error has occured, please try again';
 
         // Set the formError field with the error text
         document.querySelector("#" + formId + " .formError").innerHTML = error;
@@ -128,7 +128,7 @@ app.bindForms = function () {
 
 // Form response processor
 app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
-  var functionToCall = false;
+  let functionToCall = false;
   if (formId == 'accountCreate') {
     // @TODO Do something here now that the account has been created successfully
     console.log('Create account form submitted');
