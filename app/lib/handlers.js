bd03f0c5ec16d1717e6e7f1b556d00bdbb4dc4fd
Bryan Sharpley <motosharpley@gmail.com>
2018-07-29 16:45:16 -0500

    add sessionCreate handler

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -62,6 +62,7 @@ handlers.index = function(data,callback){
   }
 }
 
+
 // Create Account
 handlers.accountCreate = function(data,callback){
     // Reject any method that is not GET
@@ -93,9 +94,44 @@ handlers.accountCreate = function(data,callback){
       })
     } else {
       callback(405,undefined,'html');
-    }
-  
+    }  
 }
+
+
+// Create New Session
+handlers.sessionCreate = function(data,callback){
+  // Reject any method that is not GET
+  if(data.method == 'get'){
+
+    // Prepare data for interpolation
+    let templateData = {
+      'head.title' : 'Login to your account',
+      'head.description' : 'Please enter your phone number and password to access your account ',
+      'body.class' : 'sessionCreate'
+    };
+
+
+    // Read in a template as a string
+    helpers.getTemplate('sessionCreate',templateData,function(err,str){
+      if(!err && str){
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str,templateData,function(err,str){
+          if(!err && str){
+            // Return that page as HTML
+            callback(200,str,'html');
+          } else {
+            callback(500,undefined,'html');
+          }
+        })
+      } else {
+        callback(500,undefined,'html');
+      }
+    })
+  } else {
+    callback(405,undefined,'html');
+  }  
+}
+
 
 // Favicon
 handlers.favicon = function(data,callback){
