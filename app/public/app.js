6c37bd4406ddb56723529b2026329348906d4081
Bryan Sharpley <motosharpley@gmail.com>
2018-08-02 18:00:38 -0500

    refactor bind forms for check create

--- app/public/app.js
+++ app/public/app.js
@@ -139,11 +139,28 @@ app.bindForms = function () {
         let elements = this.elements;
         for (let i = 0; i < elements.length; i++) {
           if (elements[i].type !== 'submit') {
-            let valueOfElement = elements[i].type == 'checkbox' ? elements[i].checked : elements[i].value;
-            if (elements[i].name == '_method') {
+            // Determine class of element and set value accordingly
+            let classOfElement = typeof(elements[i].classList.value) == 'string' && elements[i].classList.value.length > 0 ? elements[i].classList.value : '';
+            let valueOfElement = elements[i].type == 'checkbox' && classOfElement.indexOf('multiselect') == -1 ? elements[i].checked : classOfElement.indexOf('intval') == -1 ? elements[i].value : parseInt(elements[i].value);
+            let elementIsChecked = elements[i].checked;
+            // Override the method of the form if the input's name is _method
+            let nameOfElement = elements[i].name;
+            if(nameOfElement == '_method'){
               method = valueOfElement;
             } else {
-              payload[elements[i].name] = valueOfElement;
+              // Create an payload field named "method" if the elements name is actually httpmethod
+              if(nameOfElement == 'httpmethod'){
+                nameOfElement = 'method';
+              }
+              // If the element has the class "multiselect" add its value(s) as array elements
+              if(classOfElement.indexOf('multiselect') > -1){
+                if(elementIsChecked){
+                  payload[nameOfElement] = typeof(payload[nameOfElement]) == 'object' && payload[nameOfElement] instanceof Array ? payload[nameOfElement] : [];
+                  payload[nameOfElement].push(valueOfElement);
+                }
+              } else {
+                payload[nameOfElement] = valueOfElement;
+              }
             }
           }
         }
@@ -218,6 +235,11 @@ app.formResponseProcessor = function (formId, requestPayload, responsePayload) {
    if(formId == 'accountEdit3'){
     app.logUserOut(false);
     window.location = '/account/deleted';
+  }
+
+  // If the user just created a new check successfully, redirect back to the dashboard
+  if(formId == 'checksCreate'){
+    window.location = '/checks/all';
   }
 
 };
