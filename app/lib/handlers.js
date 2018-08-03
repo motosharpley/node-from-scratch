d93b533e62020341bf53cd113b279a0b0803f222
Bryan Sharpley <motosharpley@gmail.com>
2018-08-02 19:30:34 -0500

    add checksList aka dashboard handler

--- app/lib/handlers.js
+++ app/lib/handlers.js
@@ -263,6 +263,39 @@ handlers.checksCreate = function(data,callback){
 }
 
 
+// Dashboard (view all checks)
+handlers.checksList = function(data,callback){
+  // Reject any method that is not GET
+  if(data.method == 'get'){
+
+    // Prepare data for interpolation
+    let templateData = {
+      'head.title' : 'Dashboard',
+      'body.class' : 'checksList'
+    };
+
+    // Read in a template as a string
+    helpers.getTemplate('checksList',templateData,function(err,str){
+      if(!err && str){
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str,templateData,function(err,str){
+          if(!err && str){
+            // Return that page as HTML
+            callback(200,str,'html');
+          } else {
+            callback(500,undefined,'html');
+          }
+        })
+      } else {
+        callback(500,undefined,'html');
+      }
+    })
+  } else {
+    callback(405,undefined,'html');
+  }  
+}
+
+
 
 // Favicon
 handlers.favicon = function(data,callback){
