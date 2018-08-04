584c80f6f8a5a0492514ae6aa497e6f811805d74
Bryan Sharpley <motosharpley@gmail.com>
2018-08-03 20:42:31 -0500

    add start message and interface object / prompt

--- app/lib/cli.js
+++ app/lib/cli.js
@@ -18,5 +18,20 @@ const cli = {};
 // Init Script
 cli.init = function(){
   // Send the start message to the console, in dark blue
-  
+  console.log('\x1b[34m%s\x1b[0m','The CLI is running');
+
+  // Start the interface
+  let _interface = readline.createInterface({
+    input: process.stdin,
+    output : process.stdout,
+    prompt : '>'
+  });
+
+  // Create an initial prompt
+  _interface.prompt();
+
 }
+
+
+
+module.exports = cli;
