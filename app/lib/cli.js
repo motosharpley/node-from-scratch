72f348ca8fe4151e8ed06b6d5e23063d4bb7fe16
Bryan Sharpley <motosharpley@gmail.com>
2018-08-03 21:27:27 -0500

    scaffold cli commands  and process input

--- app/lib/cli.js
+++ app/lib/cli.js
@@ -13,6 +13,43 @@ const e = new _events();
 // Instantiate the CLI module object
 const cli = {};
 
+// Input Processor
+cli.processInput = function(str){
+  str = typeof(str) == 'string' && str.trim().length > 0 ? str.trim() : false;
+  // Only process the input if the user actually wrote something. Otherwise ignore it.
+  if(str){
+    // Codify the unique strings that identify the unique questions allowed to be asked
+    const uniqueInputs = [
+      'man',
+      'help',
+      'exit',
+      'stats',
+      'list users',
+      'more user info',
+      'list checks',
+      'more check info',
+      'list logs',
+      'more log info'
+    ];
+
+    // Go through the possible inputs, emit an event when a match is found
+    let matchFound = false;
+    let counter = 0;
+    uniqueInputs.some(function(input){
+      if(str.toLowerCase().indexOf(input) > -1){
+        matchFound = true;
+        // Emit an event matching the unique input, and include the full string given by the user
+        e.emit(input,str);
+        return true;
+      }
+    });
+
+    // If no match is found, tell the user to try again
+    if(!matchFound){
+      console.log(`Command ${str} not found, Please try again`);
+    }
+  }
+}
 
 
 // Init Script
@@ -29,6 +66,20 @@ cli.init = function(){
 
   // Create an initial prompt
   _interface.prompt();
+
+  // Handle each line of input seperately
+  _interface.on('line',function(str){
+    // Send to the input processor
+    cli.processInput(str);
+
+    // Re-initialize the prompt
+    _interface.prompt();
+  });
+
+  // If the user stops the CLI , kill the associated process
+  _interface.on('close',function(){
+    process.exit(0);
+  });
 
 }
 
