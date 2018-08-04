33a1a8549d781ae5a0ba43bc1aaf446cfa62d48e
Bryan Sharpley <motosharpley@gmail.com>
2018-08-03 20:24:06 -0500

    set up initial deps and cli object

---
+++ app/lib/cli.js
@@ -0,0 +1,22 @@
+/**
+ * CLI-Related Tasks
+ */
+
+//  Dependencies
+const readline = require('readline');
+const util = require('util');
+const debug = util.debuglog('cli');
+const events = require('events');
+class _events extends events{};
+const e = new _events();
+
+// Instantiate the CLI module object
+const cli = {};
+
+
+
+// Init Script
+cli.init = function(){
+  // Send the start message to the console, in dark blue
+  
+}
