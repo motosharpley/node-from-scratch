ec0183f2286a4a1f60f86d1e2941a777c8c1f60f
Bryan Sharpley <motosharpley@gmail.com>
2018-08-03 20:21:29 -0500

    add cli  lib and init

--- app/index.js
+++ app/index.js
@@ -6,6 +6,7 @@
 // Dependencies
 const server = require('./lib/server');
 const workers = require('./lib/workers');
+const cli = require('./lib/cli');
 
 // Declare the app
 const app = {};
@@ -17,6 +18,11 @@ app.init = function() {
 
   // Start the workers
   workers.init();
+
+  // Start the CLI but make sure it starts last
+  setTimeout(function(){
+    cli.init();
+  },50);
 }
 
 // Execute
