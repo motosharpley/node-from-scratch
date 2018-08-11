fc12798139e5a6a51b899cdfced86e835c331f61
Bryan Sharpley <motosharpley@gmail.com>
2018-08-11 14:36:45 -0500

    fix done cb on ping test and add api/users 400 test

--- app/test/api.js
+++ app/test/api.js
@@ -45,6 +45,15 @@ api['app.init should start without throwing'] = function(done){
 api['/ping should respond to GET with 200'] = function(done){
   helpers.makeGetRequest('/ping',function(res){
     assert.equal(res.statusCode,200);
+    done();
+  });
+};
+
+// Make a request to /api/users
+api['/api/users should respond to GET with 400'] = function(done){
+  helpers.makeGetRequest('/api/users',function(res){
+    assert.equal(res.statusCode,400);
+    done();
   });
 };
 
