ecc04968a3cc292d4f59c9b993f49915ef6adb9f
Bryan Sharpley <motosharpley@gmail.com>
2018-08-11 12:38:00 -0500

    add get request helper for integration tests

--- app/test/api.js
+++ app/test/api.js
@@ -11,6 +11,28 @@ const config = require('./../lib/config');
 // Container for tests
 const api = {};
 
+// Helpers
+const helpers = {};
+helpers.makeGetRequest = function(path,callback){
+  // Configure the request details
+  let requestDetails = {
+    'protocol' : 'http',
+    'hostname' : 'localhost',
+    'port' : config.httpPort,
+    'method' : 'GET',
+    'path' : path,
+    'headers' : {
+      'Content-Type' : 'application/json'
+    }
+  };
+  // Send the request
+  let req = http.request(requestDetails,function(res){
+    callback(res);
+  });
+  req.end();
+};
+
+
 
 // Export tests to the runner
 module.exports = api;
